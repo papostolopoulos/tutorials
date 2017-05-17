dc367eae14a7939f128beca4fb784125f4eea3a9
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2017-05-16 22:10:28 -0700

    commit

--- javascript/tony_alicea-javascript_weird_parts/app.js
+++ javascript/tony_alicea-javascript_weird_parts/app.js
@@ -1,6 +1,29 @@
 console.log("WORKING");
 var a = "Hello world"
 
-function b() {
-
+function addNumbers (number1) {
+  return function (number2) {
+    return number1 + number2;
+  }
 }
+
+function sayHi(language){
+  if (language === "english") {
+    console.log('english');
+    return function(name) {
+      return("Hello" + " " + name)
+    }
+  }
+  else if (language === "spanish") {
+    console.log('spanish');
+    return function(name) {
+      return("Hola" + " " + name)
+    }
+  }
+}
+
+var sayHiEn = sayHi('english');
+var sayHiEs = sayHi('spanish');
+
+console.log(sayHiEn("Rena"));
+console.log(sayHiEs("Rena"));
