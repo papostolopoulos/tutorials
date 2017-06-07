2d5c8401003439c0af1e218c23dcb311d826c969
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2017-06-07 00:58:57 -0700

    adding stuff

---
+++ node/tony_alicea-learn_and_understand_node/D2_FirstClassFunctions/Starter/app.js
@@ -0,0 +1,25 @@
+// function statement
+function greet() {
+  console.log('hi');
+}
+greet();
+
+//functions are first-class
+function logGreeting(fn) {
+  fn();
+}
+logGreeting(greet);
+
+//function expression
+var greetMe = function () {
+  console.log("Hi Paris");
+}
+greetMe();
+
+//it is first-class
+logGreeting(greetMe);
+
+//use a function expression on the fly
+logGreeting(function () {
+  console.log("Hi there Paris!");
+})
---
+++ node/tony_alicea-learn_and_understand_node/D5_PrototypalInheritance/Starter/app.js
@@ -0,0 +1,13 @@
+// Your Javascript Code Goes Here
+
+function Person (firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+}
+
+var paris = new Person("Paris", "Apostolopoulos");
+var george = new Person("George", "Michos");
+
+Person.prototype.greet = function () {
+  console.log("Hello", this.firstName, this.lastName);
+}
