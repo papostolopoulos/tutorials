774d7c3c6d2fc985666d6ad81cda4401913f8378
Paraskevas Apostolopoulos <papost@fb.com>
2018-05-24 08:08:40 -0700

    Changed location of algos tutorials

--- python/helloWorld.py
+++ python/helloWorld.py
@@ -1,1 +1,21 @@
 print("Hello world")
+
+# Easy
+# Write a program to add two numbers
+def addition(a, b):
+    return a + b
+
+print(addition(5, 5))
+# Medium
+# Solve this equation manually.
+# 2 + 3 * 4
+
+
+# Now write a program to solve this.
+# Did you get the same answer?
+
+
+# Hard
+# Use a logical operator in between two comparison operators
+# and return the
+# value as true
