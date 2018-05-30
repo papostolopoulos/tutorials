dac77b7e0f1feb2808223c17bae72b1f815a9198
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2018-05-29 22:21:16 -0700

    did some LinkedLists tutorials in algos

---
+++ algos/eric_traub-learning_algorithms_in_javascript/001-linkedLists.js
@@ -0,0 +1,41 @@
+/*
+A linked list is a linear data structure where each element is a separate object.
+Each element (we will call it a node) of a list is comprising of two items -
+the data and a reference to the next node.
+*/
+
+function LinkedList(){
+  this.head = null;
+  this.tail = null;
+}
+
+function Node(value, next, previous) {
+  this.value = value;
+  this.next = next;
+  this.previous = previous;
+}
+
+LinkedList.prototype.addToHead = function(value) {
+  var newNode = new Node(value, this.head, null);
+  if (this.head) this.head.prev = newNode;
+  else this.tail = newNode;
+  this.head = newNode;
+};
+
+var ll = new LinkedList();
+
+ll.addToHead(100);
+ll.addToHead(200);
+ll.addToHead(300);
+
+LinkedList.prototype.addToTail = function(value) {
+  var newNode = new Node(value, null, this.tail);
+  if (this.tail) this.tail.next = newNode;
+  else this.head = newNode;
+  this.tail = newNode;
+};
+
+var myLL = new LinkedList();
+myLL.addToTail(10);
+myLL.addToTail(20);
+myLL.addToTail(30);
