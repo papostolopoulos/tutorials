10261760138ef5656d2e1e31cf4fe4a1eac9d47c
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2018-08-10 20:45:48 -0700

    commit

--- react/fullstack-react-code/voting_app/public/js/app.js
+++ react/fullstack-react-code/voting_app/public/js/app.js
@@ -1,1 +1,9 @@
-
+class ProductList extends React.component {
+  render() {
+    return (
+      <div className='ui unstackable items'>
+        Hello, friend! I am a basic React component.
+      </div>
+    );
+  }
+}
