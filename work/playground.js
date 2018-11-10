329eaa21a8076b8f48973bfef01f42fc50fb5caf
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2018-11-09 22:41:51 -0800

    added some of the iframe

--- work/playground.js
+++ work/playground.js
@@ -73,6 +73,21 @@ function findNextDate(startDateStr, dayOfWeekStr) {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+var str = "Sign up for texts& get 25% off your first order! Exclusions apply.";
+
 function transform(data) {
   if(!data) return null;
 
@@ -99,12 +114,89 @@ function transform(data) {
   var replaceStrings = [
     {oldStr: /\spercent/g, newStr: "%"},
     {oldStr: /(\$)\s(\d)/, newStr: "$1$2"},
-    {oldStr: /\spercent/g, newStr: "%"}
+    {oldStr: /([A-z])(&)/g, newStr: "$1 $2"}
   ]
 
   replaceStrings.forEach(function(el) {
     data = data.replace(el.oldStr, el.newStr);
-  })
+  });
+
+ return data.trim();
+}
+
+
+
+
+function transform(data) {
+ if (data) return data.replace(/\spercent/g,"%").replace(/\,\sOut of Store.*|\,\sFinancing.*|\,\sExclusions\,|Exclusions\,|Exclusions apply\.|\,\s*Shop\s*All\s*\w+|\, Join Now|\, Shop Now|Terms.*|\,\sExclusions and Details/g,"").replace(/(\$)\s(\d)/,"$1$2").trim();
+ else return null;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function transform(data) {
+  if(!data) return null;
+
+
+  var removeStrings = [
+    /[\*©®ǂ†→§™¹›]/g,
+    /Valid\sthrough.*/i,
+    /Valid\s((\d{1,2}\/){2}\d{2}–?){2}.*/i,
+    /Valid\sfor\sone-time\suse(.*)?/i,
+    /coupon\svalid\sfor.*/i,
+    /Browse\sBottles$/i,
+    /Browse\sBeers$/i,
+    /Shop\sNow$/i,
+    /Shop\sNow\sUse\sBarcode.*$/i,
+    /Use\sBarcode\sTo\sRedeem.*/,
+    /^Plus,?\s/i,
+    /^\d\sDays?\sOnly!?\sEnds\s\d{1,2}\/\d{1,2}/i,
+    /Free\sitem\smust\sbe\sof\sequal\sor\slesser\svalue(.*)?/,
+    /worldmarket.com(\/[a-z]+)?/i,
+    /Find\sA\sStore$/i,
+    /Sign\sIn$/,
+    /Join\sNow$/,
+    /MEMBER\sPRICING:?/,
+    /HALLMARK\sCHANNEL\sMOVIE\sSWEEPSTAKES.*/,
+    /to\sredeem\soffer\..*/,
+    /Shopper\sReward\scredits\searned.*/,
+    /^Or\s/,
+    /^Plus\s/
+  ]
+
+  removeStrings.forEach(function(el) {
+    data = data.replace(el, "");
+  });
+
+
+
+  var replaceStrings = [
+    {oldStr:/(.*):/, newStr: "$1"}
+  ];
+
+  replaceStrings.forEach(function(el) {
+    data = data.replace(el.oldStr, el.newStr);
+  });
 
  return data.trim();
 }
