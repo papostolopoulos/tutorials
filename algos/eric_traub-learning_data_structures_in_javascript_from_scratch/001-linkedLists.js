827410a63fac3f0cd9d510107e6ec2653e0d91ef
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2019-01-20 22:45:25 -0800

    linked lists

--- algos/eric_traub-learning_data_structures_in_javascript_from_scratch/001-linkedLists.js
+++ algos/eric_traub-learning_data_structures_in_javascript_from_scratch/001-linkedLists.js
@@ -20,40 +20,73 @@ LinkedList.prototype.addToHead = function(value) {
   //linked list.
   //This new node has to be placed at the beginning of the Linked list. Therefore,
   //the NEXT parameter is the current HEAD of the linked list.
-  //The PREV parameter should be null because this node will bedome the HEAD of the
+  //The PREV parameter should be NULL because this node will become the HEAD of the
   //linked list and therefore there will be no other nodes preceding this one
   var newNode = new Node(value, this.head, null);
+
+  //2. Position head
   //If there is a node in the linkedList, then we have to add the new node to the
-  //old head node. We have to make sure that the NEXT property of the new node
-  //is pointing at the old head, and the PREVIOUS property of the old head is
-  //pointing at the new Node
-  //Then we have to make sure that the linked list Head pointer or this.head is
-  //referencing the new node
+  //old head node. We have to make sure that
+  //   ---the NEXT property of the new node is pointing at the old head
+  //      (done already when we defined the new NOde),
+  //   ---and the PREVIOUS property of the old head is pointing at the new Node
+
   if(this.head !== null) this.head.prev = newNode; //LinkedList is not empty
 
-  //Since the linked list is empty, the new node has to become the tail of the list
-  //(and at the next likne of code it also becomes the head of the list)
+  //Then we have to make sure that the linked list Head pointer (this.head
+  //LinkedList.head) is referencing the new node (this.head = newNode)
+
+  //Since the linked list is empty, the new node has to alse become the tail of the list
   else this.tail = newNode;
 
   //Regardless of if there are nodes existing in the linked list, we have to make
   //sure that the head of the linked list equals to the new node since the new node
   //is the new head
   this.head = newNode;
-}
+};
 
 
 
-//What do we do when the linked list does not have any nodes to it versus what do
-//we do if there nodes present to the linkedlist.
 //If there is no node in the linked list, then we add the node and we have the
 // head and the tail point to the new node. There is no connecting nodes so
 //this new node is both the head and the tail of the LinkedList
 
 
 
+LinkedList.prototype.addToTail = function(value) {
+  var newNode = new Node(value, null, this.tail);
+
+  if (this.tail) this.tail.next = newNode;
+  else this.head = newNode;
+  this.tail = newNode;
+};
 
 
+LinkedList.prototype.removeFromHead = function() {
+  if (!this.head) return;
+  if (this.head && !this.head.next) {
+    this.head = null;
+    this.tail = null;
+  }
+  else {
+    this.head = this.head.next;
+    this.head.prev = null;
+    return this.head.value;
+  }
+}
 
+LinkedList.prototype.removeFromTail = function() {
+  if(!this.tail) return;
+  if (this.tail && !this.tail.prev) {
+    this.tail = null;
+    this.head = null;
+  }
+  else {
+    this.tail = this.tail.prev;
+    this.tail.next = null;
+    return this.tail.value;
+  }
+}
 
 
 
