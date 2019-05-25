9dd5e1fee33c912622435479473be8dbe4ba8f77
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2019-05-24 18:38:20 -0700

    node tutorial

---
+++ node/maximilian_schwarzmuller-nodeJS_the_complete_guide/playground.js
@@ -0,0 +1,27 @@
+const fetchData = () => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Resolve - Done");
+    }, 1500);
+  });
+  return promise;
+};
+
+setTimeout(() => {
+  console.log("Timeout is run!");
+  fetchData()
+    .then(text => {
+      console.log("text");
+
+      console.log(text);
+      return fetchData();
+    })
+    .then(text2 => {
+      console.log("text2");
+
+      console.log(text2);
+    });
+}, 2000);
+
+console.log("Hello!");
+console.log("Hi. I am after Hello");
