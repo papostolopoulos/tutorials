3e3b6ada3c5721c5e8c6bb0214cedf1b21c099c7
Paris Apostolopoulos <papostolopoulos@gmail.com>
2025-08-22 18:48:02 -0700

    start.py update

--- python/start.py
+++ python/start.py
@@ -1,6 +1,16 @@
 # start.py
 # This is the entry Python file for your project.
 
+from datetime import datetime
+
+#define the dates
+christmas_date = datetime(datetime.now().year, 12, 25)
+birthday_date = datetime(year=2026, month=5, day=17)  # Replace with your birthday
+
+#Calculate the difference in days
+delta = birthday_date - christmas_date
+print(f"There are {delta.days} days between Christmas and my birthday.")
+
 def main():
     print("Hello, world!")
 
